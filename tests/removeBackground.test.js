f62f3d1cdc8fc30a7febb7c4175416d642ee8d81
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-11-11 04:23:28 +0000

    hope this works

--- tests/removeBackground.test.js
+++ tests/removeBackground.test.js
@@ -9,7 +9,7 @@ describe('removeBackgroundColor', () => {
   const expectedOutputPath = path.join(__dirname, 'test-assets', 'expected-output.png');
 
   beforeEach(() => {
-    // Clean up the output file beforsdfsdfe each test
+    // Clean up the output filefdgfdg beforsdfsdfe each test
     if (fs.existsSync(outputPath)) {
       fs.unlinkSync(outputPath);
     }
