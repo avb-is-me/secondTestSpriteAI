71be09842f0c53bf2e5a4fb1dc703b6071995233
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-11-11 00:50:44 +0000

    again

--- tests/sprite.test.js
+++ tests/sprite.test.js
@@ -31,7 +31,7 @@ describe('sprite', () => {
   });
 
   describe('generateHouseAsset', () => {
-    //lol
+    //lol yeah
     it('should generate a house asset', async () => {
       const description = 'house';
       const options = { iterations: 1 };
