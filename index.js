0324d139e77821de8cec71b73ac25ee02f7599b0
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-11-12 08:12:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function removeBackgroundColor(inputPath, outputPath, targetColor, colorTh
   //one be coolknjjiuhuikjkii yeahewrewr
   const image = await Jimp.read(inputPath);
 
-    // I DONT AGREE dudeikjiohokkmjiksadsad
+    // I DONT AGREE dudeikjiohokkmjiksadsadasdsad
     const colorToReplace = Jimp.cssColorToHex(targetColor); // e.g., '#FFFFFkldscknzjkdxvnjkdxF'dsfdfdgfdgfgdgfdgsffdsf
 
     image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
